9318ae44d13351104764bed0b9f1b4d9640aae96
Siva <sivasankar.sivakripa123@gmail.com>
2026-08-11 20:44:52 +0530

    cleanup

--- apps/desktop_test/lib/core/database/migrations/migration_v2.dart
+++
@@ -1,8 +0,0 @@
-import 'package:drift/drift.dart';
-import '../brain_database.dart';
-
-class MigrationV2 {
-  static Future<void> onUpgrade(BrainDatabase db, Migrator m) async {
-    await m.createTable(db.projectTable);
-  }
-}
--- apps/desktop_test/lib/core/theme/loader/theme_loader.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-import 'package:brainstorm/core/theme/model/app_theme.dart';
-
-abstract interface class ThemeLoader {
-  Future<AppTheme> loadAsset(String asset);
-  Future<AppTheme> loadFile(File file);
-  Future<void> save(File file, AppTheme theme);
-}
-
